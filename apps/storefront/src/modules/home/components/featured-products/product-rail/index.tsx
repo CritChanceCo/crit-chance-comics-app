7a20687e51d9468efd02b58a4519341718618888
Catch Fehst <catchfehst@Catchs-MacBook-Air.local>
2026-09-25 14:48:26 -0700

    style(storefront): add logo and searchbox

--- apps/storefront/src/modules/home/components/featured-products/product-rail/index.tsx
+++ apps/storefront/src/modules/home/components/featured-products/product-rail/index.tsx
@@ -2,6 +2,9 @@
 
 import { HttpTypes } from "@medusajs/types"
 import ProductPreview from "@modules/products/components/product-preview"
+
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import Image from "next/image"
 import { searchClient } from "@lib/config"
 import { InstantSearch, Hits, SearchBox, Pagination } from "react-instantsearch"
 
@@ -11,23 +14,44 @@ export default function MeilisearchProductRail({
   region: HttpTypes.StoreRegion
 }) {
   return (
-    <div className="py-12 content-container small:py-24">
-      <InstantSearch indexName="comics" searchClient={searchClient}>
-        <div className="mb-8 flex justify-end">
-          <SearchBox />
+    <div className="flex w-full flex-col">
+      <InstantSearch
+        searchClient={searchClient}
+        indexName={process.env.NEXT_PUBLIC_MEILISEARCH_INDEX_NAME}
+      >
+        <div className="flex flex-row items-baseline justify-between gap-10 py-10 content-container">
+          <LocalizedClientLink
+            href="/"
+            className="xl:w-[250%] small:block"
+            data-testid="nav-store-link"
+          >
+            <Image
+              src="/images/crit-chance-comics.svg"
+              alt="Crit Chance Comics Logo"
+              width={3650}
+              height={1945}
+              className="object-contain"
+              priority
+            />
+          </LocalizedClientLink>{" "}
+          <SearchBox
+            placeholder="SEARCH"
+            className="custom-search-box w-full"
+          />
         </div>
-
-        <Hits
-          hitComponent={({ hit }: { hit: HttpTypes.StoreProduct }) => (
-            <div key={hit.id}>
-              <ProductPreview
-                product={hit}
-                region={region}
-                isFeatured={false}
-              />
-            </div>
-          )}
-        />
+        <div className="content-container">
+          <Hits
+            hitComponent={({ hit }: { hit: HttpTypes.StoreProduct }) => (
+              <div key={hit.id}>
+                <ProductPreview
+                  product={hit}
+                  region={region}
+                  isFeatured={false}
+                />
+              </div>
+            )}
+          />
+        </div>
         <Pagination showLast={true} />
       </InstantSearch>
     </div>

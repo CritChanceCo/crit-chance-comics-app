a077674939131d32462184872e9342174a73f987
Catch Fehst <catchfehst@Catchs-MacBook-Air.local>
2026-09-25 14:48:01 -0700

    refactor(storefront): remove hero

--- apps/storefront/src/app/[countryCode]/(main)/page.tsx
+++ apps/storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next"
 
 import FeaturedProducts from "@modules/home/components/featured-products"
-import Hero from "@modules/home/components/hero"
 import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
@@ -30,8 +29,7 @@ export default async function Home(props: {
 
   return (
     <>
-      <Hero />
-      <div className="py-12 bg-ui-bg-subtle">
+      <div className="bg-ui-bg-subtle py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
         </ul>
